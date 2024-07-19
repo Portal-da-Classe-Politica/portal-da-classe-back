56867c4bde4e105a13946e744641f16a1c328182
AcacioTelechi <acaciotelechi@gmail.com>
2024-07-19 01:23:15 -0300

    refactor: change route param dimension to query

--- src/routes/noauth/Cruzamentos.js
+++ src/routes/noauth/Cruzamentos.js
@@ -4,8 +4,10 @@ const CruzamentosController = require("../../controllers/CruzamentosController")
 
 // router.get("/", CruzamentosController.getPossibilities)
 
-router.get("/candidates-profile/:dimension/by-gender/", CruzamentosController.getCandidatesByGender)
+router.get("/candidates-profile/by-gender/", CruzamentosController.getCandidatesByGender)
 
-router.get("/candidates-profile/:dimension/by-year/", CruzamentosController.getCandidatesByYear)
+router.get("/candidates-profile/by-year/", CruzamentosController.getCandidatesByYear)
+
+router.get("/candidates-profile/by-occupation/", CruzamentosController.getCandidatesByOcupations)
 
 module.exports = router
