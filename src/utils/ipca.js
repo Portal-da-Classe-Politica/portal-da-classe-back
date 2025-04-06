4b3ab5df029065dfd3975f40fbbafd62f94debbf
Luiz D Avila <luizdavilaf@gmail.com>
2025-04-06 12:14:16 -0300

    fix: ajustando ipca

--- src/utils/ipca.js
+++ src/utils/ipca.js
@@ -1,23 +1,25 @@
 /**
  *Tabela 1737 - IPCA - Série histórica com número-índice, variação mensal e variações acumuladas em 3 meses, em 6 meses, no ano e em 12 meses (a partir de dezembro/1979)
 Variável - IPCA - Número-índice (base: dezembro de 1993 = 100) (Número-índice)
-o valor é calculado atraves do numero indice de dezembro de 2023 / numero indice do mes de dezembro de referencia
+o valor é calculado atraves do numero indice de dezembro de 2024 / numero indice do mes de dezembro de referencia
+https://sidra.ibge.gov.br/tabela/1737
 
  *  */
 const fatoresDeCorreção = {
-    "1998": 4.64495268138801,
-    "2000": 4.02339810035225,
-    "2002": 3.32058849483768,
-    "2004": 2.82346639321028,
-    "2006": 2.59011108774211,
-    "2008": 2.34137497148151,
-    "2010": 2.11936893948165,
-    "2012": 1.88017909983733,
-    "2014": 1.66835063278044,
-    "2016": 1.41827794878238,
-    "2018": 1.32793332562184,
-    "2020": 1.21808477157999,
-    "2022": 1.04621190005082,
+    "1998": 4.86935948429571,
+    "2000": 4.21777637855144,
+    "2002": 3.48101265822785,
+    "2004": 2.95987360979107,
+    "2006": 2.71524445039292,
+    "2008": 2.45449140297145,
+    "2010": 2.22175982277237,
+    "2012": 1.97101425137267,
+    "2014": 1.74895193430315,
+    "2016": 1.48679774692715,
+    "2018": 1.39208839727013,
+    "2020": 1.27693284345726,
+    "2022": 1.09675645534739,
+    "2024": 1.00000000000000,
 }
 
 function atualizarValor(valorOriginal, anoDoacao) {
