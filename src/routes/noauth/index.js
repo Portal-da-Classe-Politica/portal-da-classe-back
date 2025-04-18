31291c537a0aae52e4930ef9d26360283c338717
Luiz D Avila <luizdavilaf@gmail.com>
2025-04-18 03:44:13 -0300

    ajuste

--- src/routes/noauth/index.js
+++ src/routes/noauth/index.js
@@ -19,7 +19,7 @@ router.use("/indicadores", Indicadores)
 const Partido = require("./Partido")
 router.use("/party", Partido)
 
-const analises = require("./roberto")
+const analises = require("./Analises")
 router.use("/analises", analises)
 
 module.exports = router
