1c950686f3700ad257a4b00338ade03dcbfcaa36
Luiz D Avila <luizdavilaf@gmail.com>
2025-04-18 03:44:46 -0300

    sdsa

--- src/routes/noauth/index.js
+++ src/routes/noauth/index.js
@@ -19,7 +19,7 @@ router.use("/indicadores", Indicadores)
 const Partido = require("./Partido")
 router.use("/party", Partido)
 
-const analises = require("./Analises")
+const analises = require("./roberto")
 router.use("/analises", analises)
 
 module.exports = router
