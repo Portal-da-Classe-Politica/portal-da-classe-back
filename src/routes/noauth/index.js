2e0edf54656c0f1a39c9e212c17aab0d25f86aeb
Luiz D Avila <luizdavilaf@gmail.com>
2025-04-18 03:50:44 -0300

    teste steasjhesaejiasjieasjki esaj

--- src/routes/noauth/index.js
+++ src/routes/noauth/index.js
@@ -19,7 +19,7 @@ router.use("/indicadores", Indicadores)
 const Partido = require("./Partido")
 router.use("/party", Partido)
 
-const analises = require("./roerre")
+const analises = require("./naddamais")
 router.use("/analises", analises)
 
 module.exports = router
